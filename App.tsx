2b88f7ce86bd8f945c1d1b014faed785d9658ebc
derneuere <faridani.rad@gmail.com>
2022-10-17 17:31:35 +0200

    Implement MVP

--- App.tsx
+++ App.tsx
@@ -8,113 +8,117 @@
  * @format
  */
 
-import React, {type PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import React from 'react';
+import {Button, Text, View} from 'react-native';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  // This App implements a calculator
+  const [result, setResult] = React.useState(0);
+  const [firstNumber, setFirstNumber] = React.useState(0);
+  const [secondNumber, setSecondNumber] = React.useState(0);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const add = () => {
+    setResult(firstNumber + secondNumber);
+    setFirstNumber(firstNumber + secondNumber);
+  };
+
+  const subtract = () => {
+    setResult(firstNumber - secondNumber);
+    setFirstNumber(firstNumber - secondNumber);
+  };
+
+  const multiply = () => {
+    setResult(firstNumber * secondNumber);
+    setFirstNumber(firstNumber * secondNumber);
+  };
+
+  const divide = () => {
+    setResult(firstNumber / secondNumber);
+    setFirstNumber(firstNumber / secondNumber);
+  };
+
+  const setNumber = (number: number) => {
+    if (firstNumber === 0) {
+      setFirstNumber(number);
+    } else {
+      setSecondNumber(number);
+    }
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <View>
+      <Text>Calculator</Text>
+      <Text>Result: {result}</Text>
+      <Text>First Number: {firstNumber}</Text>
+      <Text>Second Number: {secondNumber}</Text>
+      <Button
+        title="1"
+        onPress={() => {
+          setNumber(1);
+        }}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+      <Button
+        title="2"
+        onPress={() => {
+          setNumber(2);
+        }}
+      />
+      <Button
+        title="3"
+        onPress={() => {
+          setNumber(3);
+        }}
+      />
+      <Button
+        title="4"
+        onPress={() => {
+          setNumber(4);
+        }}
+      />
+      <Button
+        title="5"
+        onPress={() => {
+          setNumber(5);
+        }}
+      />
+      <Button
+        title="6"
+        onPress={() => {
+          setNumber(6);
+        }}
+      />
+      <Button
+        title="7"
+        onPress={() => {
+          setNumber(7);
+        }}
+      />
+      <Button
+        title="8"
+        onPress={() => {
+          setNumber(8);
+        }}
+      />
+      <Button
+        title="9"
+        onPress={() => {
+          setNumber(9);
+        }}
+      />
+
+      <Button
+        title="0"
+        onPress={() => {
+          setNumber(0);
+        }}
+      />
+
+      <Button title="Add" onPress={add} />
+      <Button title="Subtract" onPress={subtract} />
+      <Button title="Multiply" onPress={multiply} />
+      <Button title="Divide" onPress={divide} />
+    </View>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
