027fa23237170332e993069c5e00bf03f43f62cc
derneuere <faridani.rad@gmail.com>
2022-10-17 18:29:47 +0200

    Add Clear, ClearAll and Result

--- App.tsx
+++ App.tsx
@@ -11,112 +11,70 @@
 import React from 'react';
 import {Button, Text, View} from 'react-native';
 
+const NumberButton = (props: {number: number; setCalculationNumber: any}) => {
+  return (
+    <Button
+      title={props.number + ''}
+      onPress={() => props.setCalculationNumber(props.number)}
+    />
+  );
+};
+
 const App = () => {
   // This App implements a calculator
   const [result, setResult] = React.useState(0);
-  const [firstNumber, setFirstNumber] = React.useState(0);
-  const [secondNumber, setSecondNumber] = React.useState(0);
+  const [number, setNumber] = React.useState(0);
+  const [operation, setOperation] = React.useState('');
 
-  const add = () => {
-    setResult(firstNumber + secondNumber);
-    setFirstNumber(firstNumber + secondNumber);
-  };
-
-  const subtract = () => {
-    setResult(firstNumber - secondNumber);
-    setFirstNumber(firstNumber - secondNumber);
-  };
-
-  const multiply = () => {
-    setResult(firstNumber * secondNumber);
-    setFirstNumber(firstNumber * secondNumber);
-  };
-
-  const divide = () => {
-    setResult(firstNumber / secondNumber);
-    setFirstNumber(firstNumber / secondNumber);
-  };
-
-  const setNumber = (number: number) => {
-    if (firstNumber === 0) {
-      setFirstNumber(number);
+  const setCalculationNumber = (numberInput: number) => {
+    if (result == 0 && operation == '') {
+      setResult(numberInput);
     } else {
-      setSecondNumber(number);
+      setNumber(numberInput);
+      calculate();
     }
+  };
+
+  const calculate = () => {
+    setResult(eval(result + operation + number));
+  };
+
+  const clear = () => {
+    setNumber(0);
+  };
+
+  const clearAll = () => {
+    setResult(0);
+    setNumber(0);
+    setOperation('');
   };
 
   return (
     <View>
       <Text>Calculator</Text>
       <Text>Result: {result}</Text>
-      <Text>First Number: {firstNumber}</Text>
-      <Text>Second Number: {secondNumber}</Text>
-      <Button
-        title="1"
-        onPress={() => {
-          setNumber(1);
-        }}
-      />
-      <Button
-        title="2"
-        onPress={() => {
-          setNumber(2);
-        }}
-      />
-      <Button
-        title="3"
-        onPress={() => {
-          setNumber(3);
-        }}
-      />
-      <Button
-        title="4"
-        onPress={() => {
-          setNumber(4);
-        }}
-      />
-      <Button
-        title="5"
-        onPress={() => {
-          setNumber(5);
-        }}
-      />
-      <Button
-        title="6"
-        onPress={() => {
-          setNumber(6);
-        }}
-      />
-      <Button
-        title="7"
-        onPress={() => {
-          setNumber(7);
-        }}
-      />
-      <Button
-        title="8"
-        onPress={() => {
-          setNumber(8);
-        }}
-      />
-      <Button
-        title="9"
-        onPress={() => {
-          setNumber(9);
-        }}
-      />
-
-      <Button
-        title="0"
-        onPress={() => {
-          setNumber(0);
-        }}
-      />
-
-      <Button title="Add" onPress={add} />
-      <Button title="Subtract" onPress={subtract} />
-      <Button title="Multiply" onPress={multiply} />
-      <Button title="Divide" onPress={divide} />
+      <Text>
+        Calculation: {result} {operation} {number}
+      </Text>
+      <Text>First Number: {number}</Text>
+      <Text>Operation: {operation}</Text>
+      <NumberButton number={1} setCalculationNumber={setCalculationNumber} />
+      <NumberButton number={2} setCalculationNumber={setCalculationNumber} />
+      <NumberButton number={3} setCalculationNumber={setCalculationNumber} />
+      <NumberButton number={4} setCalculationNumber={setCalculationNumber} />
+      <NumberButton number={5} setCalculationNumber={setCalculationNumber} />
+      <NumberButton number={6} setCalculationNumber={setCalculationNumber} />
+      <NumberButton number={7} setCalculationNumber={setCalculationNumber} />
+      <NumberButton number={8} setCalculationNumber={setCalculationNumber} />
+      <NumberButton number={9} setCalculationNumber={setCalculationNumber} />
+      <NumberButton number={0} setCalculationNumber={setCalculationNumber} />
+      <Button title="Add" onPress={() => setOperation('+')} />
+      <Button title="Subtract" onPress={() => setOperation('-')} />
+      <Button title="Multiply" onPress={() => setOperation('*')} />
+      <Button title="Divide" onPress={() => setOperation('/')} />
+      <Button title="Result" onPress={() => calculate()} />
+      <Button title="C" onPress={() => clear()} />
+      <Button title="CE" onPress={() => clearAll()} />
     </View>
   );
 };
